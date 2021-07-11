4e1decf5384c166a9b6281b86db0f1f27da11f86
Gustave Monce <gustave.monce@outlook.com>
2021-07-11 13:58:50 +0200

    Updates

--- ColorProfile/MainPage.xaml.cs
+++ ColorProfile/MainPage.xaml.cs
@@ -16,8 +16,6 @@ namespace ColorProfile
         public MainPage()
         {
             this.InitializeComponent();
-            ImageViewScrollViewer.ChangeView(null, ImageView.Width / 2, null);
-            Window.Current.SizeChanged += Current_SizeChanged;
             Loaded += MainPage_Loaded;
             CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
             ApplicationView.GetForCurrentView().TitleBar.ButtonBackgroundColor = Colors.Transparent;
@@ -26,19 +24,7 @@ namespace ColorProfile
 
         private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-            Picture1.Width = ActualWidth;
-            Picture2.Width = ActualWidth;
-            Picture3.Width = ActualWidth;
-            Picture4.Width = ActualWidth;
             ViewModel.RefreshEverything();
-        }
-
-        private void Current_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
-        {
-            Picture1.Width = e.Size.Width;
-            Picture2.Width = e.Size.Width;
-            Picture3.Width = e.Size.Width;
-            Picture4.Width = e.Size.Width;
         }
     }
 }
